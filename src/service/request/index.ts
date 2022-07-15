fdbb4814292bddac86e83ef92b557f7acaf1db31
杨卓然 <2945532754@qq.com>
2022-07-15 13:26:10 +0800

    add:完成axios请求封装. 添加了实例拦截器和全局拦截器:1/1

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios'
+import axios, { AxiosInstance } from 'axios'
 import { HYRequestConfig, HYRequestInterceptors } from './type'
 
 class HYRequest {
@@ -10,20 +10,49 @@ class HYRequest {
     this.instance = axios.create(config)
     // 保存传入的拦截器
     this.interceptors = config.interceptors
-    // 使用请求拦截器
+
+    // 为创建出的实例对象添加拦截器
+    // 添加请求拦截器
     this.instance.interceptors.request.use(
       this.interceptors?.requestInterceptor,
       this.interceptors?.requestInterceptorCatch
     )
-    // 使用响应拦截器
+    // 添加响应拦截器
     this.instance.interceptors.response.use(
       this.interceptors?.resopnseInterceptor,
       this.interceptors?.resopnseInterceptorCatch
     )
+
+    // 为所有实例对象都添加拦截器
+    this.instance.interceptors.request.use(
+      (config) => {
+        return config
+      },
+      (err) => {
+        return err
+      }
+    )
+    this.instance.interceptors.response.use(
+      (res) => {
+        return res
+      },
+      (err) => {
+        return err
+      }
+    )
   }
 
-  request(config: AxiosRequestConfig): void {
+  request(config: HYRequestConfig): void {
+    // 执行一遍拦截器
+    if (config.interceptors?.requestInterceptor) {
+      config = config.interceptors.requestInterceptor(config)
+    }
+
     this.instance.request(config).then((res) => {
+      // 执行拦截器
+      if (config.interceptors?.resopnseInterceptor) {
+        res = config.interceptors.resopnseInterceptor(res)
+      }
       console.log(res)
     })
   }
