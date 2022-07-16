d382bca3bf0614c409c937828830032381712625
杨卓然 <2945532754@qq.com>
2022-07-16 09:57:08 +0800

    add:完成项目编码前最后的初始化:1/1

--- src/service/request/type.ts
+++ src/service/request/type.ts
@@ -1,15 +1,16 @@
 import { AxiosRequestConfig, AxiosResponse } from 'axios'
 
 // 自定义拦截器类型
-export interface HYRequestInterceptors {
+export interface HYRequestInterceptors<T = AxiosResponse> {
   requestInterceptor?: (config: AxiosRequestConfig) => AxiosRequestConfig
   requestInterceptorCatch?: (err: any) => any
-  resopnseInterceptor?: (config: AxiosResponse) => AxiosResponse
+  // resopnseInterceptor?: (config: T) => T
+  resopnseInterceptor?: (config: any) => any
   resopnseInterceptorCatch?: (err: any) => any
 }
 
 // 自定义一个请求参数类型, 来代替axios自带的AxiosRequestConfig, 在其中增加拦截器
-export interface HYRequestConfig extends AxiosRequestConfig {
-  interceptors?: HYRequestInterceptors
+export interface HYRequestConfig<T = AxiosResponse> extends AxiosRequestConfig {
+  interceptors?: HYRequestInterceptors<T>
   showLoding?: boolean
 }
